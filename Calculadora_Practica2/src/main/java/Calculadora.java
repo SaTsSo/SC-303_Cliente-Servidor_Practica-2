2e1532442b636cb412abdd4a487f49f43e2e235f
SaTsSo <stsai30160@ufide.ac.cr>
2026-06-14 15:44:43 -0600

    Se crea un nuevo Swing UI Form (Calculadora), con sus correspondientes JFrame, JPanel, JButton y JLabel

---
+++ Calculadora_Practica2/src/main/java/Calculadora.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+
+public class Calculadora extends JFrame {
+
+
+    private JPanel panelCalculadora;
+    private JButton btn7;
+    private JButton btn4;
+    private JButton btn1;
+    private JButton btnPunto;
+    private JButton btn8;
+    private JButton btn5;
+    private JButton btn2;
+    private JButton btn0;
+    private JButton btn9;
+    private JButton btn6;
+    private JButton btn3;
+    private JButton btnIgual;
+    private JButton btnDivision;
+    private JButton btnMultiplicacion;
+    private JButton btnResta;
+    private JButton btnSuma;
+    private JButton btnC;
+    private JButton btnBorrar;
+    private JLabel lblResultado;
+}
